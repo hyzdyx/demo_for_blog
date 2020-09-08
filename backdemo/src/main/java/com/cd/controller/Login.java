5650e83d065a8a8fbac1b5cb59f7877ec8676e47
DESKTOP-D2S42LH\Dell <hyzdyx@foxmail.com>
2020-09-08 15:29:41 +0800

    '1234'

--- backdemo/src/main/java/com/cd/controller/Login.java
+++ backdemo/src/main/java/com/cd/controller/Login.java
@@ -1,11 +1,12 @@
 package com.cd.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Login {
-    @RequestMapping("/login")
+    @RequestMapping("login")
     public String login(){
         return "helloworld";
     }
